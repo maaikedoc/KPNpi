2547b61e25c1d06d95a149daf4a3857a5c88833a
maaikedoc <maaikedokter@hotmail.com>
2017-01-04 15:33:45 +0000

    Original Subject File

---
+++ subject.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import socket
+import thread
+
+TCP_IP = '130.89.130.185'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+MESSAGE = "Huilen!"
+   
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()      
+
+print "received data:", data
+
+
