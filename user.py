11cada73b2253e11d9165265a2a958deba2b3bb3
maaikedoc <maaikedokter@hotmail.com>
2017-01-04 15:25:45 +0000

    Alarm Keuzemenu

--- user.py
+++ user.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import socket
-
+import string
+from gpiozero import LED
+from time import sleep
 
 TCP_IP = '130.89.130.185' #IP Address to listen on (Should be external ip for the server)
 TCP_PORT = 5005 #Port to listen on
@@ -10,6 +12,7 @@ BUFFER_SIZE = 1024  # Normally 1024, but we want fast response, How many charact
 #Catch CTRL + C
 conn = False	
 s = False
+ledalarm = LED(4)
 try:
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((TCP_IP, TCP_PORT)) #Create the server socket
@@ -28,6 +31,9 @@ try:
 			except socket.error:
 				break;
 			print "received data:", data
+			if data=="Huilen!":
+                               ledalarm.on()
+                               keuzealarm()                                        
 			conn.send(data)  # echo
 except KeyboardInterrupt:
 	print 'Closing server...'
@@ -38,3 +44,17 @@ except KeyboardInterrupt:
 	# Close listening server socket
 	if s:
 		s.close()
+
+
+def keuzealarm():
+        alarmresp = lower(raw_input("U heeft een alarm notificatie ontvangen. Toets [A] voor bellen, Toets [B] voor luisteren en Toest [C] voor negeren"))
+        if alarmresp == "a":
+                print("Ring ring...")
+        elif alarmresp == "b":
+                print("Big Brother...")
+        elif alarmresp == "c":
+                print("Talk to the hand...")
+        else:
+                print("Commando werdt niet herkend. Kies [A], [B] of [C]")
+                keuzealarm()
+
