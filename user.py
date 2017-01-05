b1e633809040c19cd8c915fa691178a39361d641
maaikedoc <maaikedokter@hotmail.com>
2017-01-05 13:48:59 +0000

    audio added

--- user.py
+++ user.py
@@ -4,22 +4,39 @@ import socket
 import string
 from gpiozero import LED
 from time import sleep
+import pygame 
 
-TCP_IP = '130.101.0.159' #IP Address to listen on (Should be external ip for the server)
+TCP_IP = '192.168.2.148' #IP Address to listen on (Should be external ip for the server)
 TCP_PORT = 5005 #Port to listen on
 BUFFER_SIZE = 1024  # Normally 1024, but we want fast response, How many characters max should be received per packet
+
+def alarmsound():
+        song = "./cow_toy.wav"
+        pygame.init()
+        pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
+        pygame.mixer.music.load("./cow_toy.wav")
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10) 
 
 def keuzealarm():
         alarmresp = raw_input("U heeft een alarm notificatie ontvangen. Toets [A] voor bellen, Toets [B] voor luisteren en Toest [C] voor negeren")
         if alarmresp == "a":
                 print("Ring ring...")
+                ledalarm.off()
+                alarmsound()
         elif alarmresp == "b":
                 print("Big Brother...")
+                ledalarm.off()
         elif alarmresp == "c":
                 print("Talk to the hand...")
+                ledalarm.off()
         else:
                 print("Commando werdt niet herkend. Kies [A], [B] of [C]")
                 keuzealarm()
+
 
 #Catch CTRL + C
 conn = False	
@@ -47,6 +64,7 @@ try:
                                ledalarm.on()
                                keuzealarm()                                        
 			conn.send(data)  # echo
+			
 except KeyboardInterrupt:
 	print 'Closing server...'
 
